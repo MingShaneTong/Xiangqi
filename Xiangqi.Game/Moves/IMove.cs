89c2c878448ba6a8fc6066edd3ebca6bb5e8b106
Ming Shane Tong <mstong800@gmail.com>
2023-01-10 22:19:03 +1300

    Creates moves

---
+++ Xiangqi.Game/Moves/IMove.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xiangqi.Game.Moves
+{
+    public interface IMove
+    {
+        public bool IsValid(Board board);
+        public void Apply(Board board);
+    }
+}
---
+++ Xiangqi.Game/Moves/Move.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xiangqi.Game.Moves
+{
+    public abstract class Move : IMove
+    {
+        private Color _color;
+        public Color Color
+        {
+            get { return _color; }
+            init { _color = value; }
+        }
+
+        public void Apply(Board board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsValid(Board board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

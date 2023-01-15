6228f6c735a32994fe76e8d7912354235bfe1bf2
MingShaneTong <63452934+MingShaneTong@users.noreply.github.com>
2023-01-15 22:19:40 +1300

    Advisor movement and tests (#8)

--- Xiangqi.Game/Pieces/Advisor.cs
+++ Xiangqi.Game/Pieces/Advisor.cs
@@ -8,6 +8,19 @@ namespace Xiangqi.Game.Pieces
 {
     public class Advisor : Piece
     {
+        public override bool IsValidMove(Board board, Position oldPosition, Position newPosition)
+        {
+            if (!Board.InCastle(Color, newPosition) || !Board.InCastle(Color, newPosition)) { return false; }
+            return 
+                Math.Abs(newPosition.Col - oldPosition.Col) == 1 && 
+                Math.Abs(newPosition.Row - oldPosition.Row) == 1;
+        }
+
+        public override bool IsValidMove(Board board, Position oldPosition, Position newPosition, IPiece pieceCaptured)
+        {
+            throw new NotImplementedException();
+        }
+
         public override string ToString()
         {
             switch (Color) 
@@ -24,16 +37,6 @@ namespace Xiangqi.Game.Pieces
         public static Advisor Of(Color color)
         {
             return new Advisor() { Color = color };
-        }
-
-        public override bool IsValidMove(Board board, Position oldPosition, Position newPosition)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool IsValidMove(Board board, Position oldPosition, Position newPosition, IPiece pieceCaptured)
-        {
-            throw new NotImplementedException();
         }
     }
 }

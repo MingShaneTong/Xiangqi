f91868ca7f634db5cfd8cb96fb3f310b79a2758b
Ming Shane Tong <mstong800@gmail.com>
2023-01-15 23:46:37 +1300

    Elephant Move

--- Xiangqi.Game/Pieces/Elephant.cs
+++ Xiangqi.Game/Pieces/Elephant.cs
@@ -8,6 +8,26 @@ namespace Xiangqi.Game.Pieces
 {
     public class Elephant : Piece
     {
+        public override bool IsValidMove(Board board, Position oldPosition, Position newPosition)
+        {
+            // must be on same side
+            if (Board.GetPositionSide(oldPosition) != Color) { return false; }
+            if (Board.GetPositionSide(newPosition) != Color) { return false; }
+
+            // must be 2 square diagonal
+            int rowDiff = Math.Abs(oldPosition.Row - newPosition.Row);
+            int colDiff = Math.Abs(oldPosition.Col - newPosition.Col);
+            if (rowDiff != 2 || colDiff != 2) { return false; }
+
+            // no obstruction
+            return !board.GetDiagonalPiecesBetween(oldPosition, newPosition).Any();
+        }
+
+        public override bool IsValidMove(Board board, Position oldPosition, Position newPosition, IPiece pieceCaptured)
+        {
+            throw new NotImplementedException();
+        }
+
         public override string ToString()
         {
             switch (Color)
@@ -23,16 +43,6 @@ namespace Xiangqi.Game.Pieces
         public static Elephant Of(Color color)
         {
             return new Elephant() { Color = color };
-        }
-
-        public override bool IsValidMove(Board board, Position oldPosition, Position newPosition)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool IsValidMove(Board board, Position oldPosition, Position newPosition, IPiece pieceCaptured)
-        {
-            throw new NotImplementedException();
         }
     }
 }
---
+++ Xiangqi.UnitTests/PiecesTests/ElephantTest/Elephant_PieceMove.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xiangqi.Game.Pieces;
+using Xiangqi.UnitTests.PiecesTests;
+
+namespace PiecesTests.ElephantTest
+{
+    [TestClass]
+    public class Elephant_PieceMove : PieceMoveTestClass<Elephant>
+    {
+        [TestMethod]
+        [DataRow("Black", 0, 2, 2, 0)]
+        [DataRow("Black", 2, 0, 4, 2)]
+        [DataRow("Black", 4, 2, 2, 4)]
+        [DataRow("Black", 2, 4, 0, 2)]
+        [DataRow("Red", 9, 6, 7, 4)]
+        [DataRow("Red", 7, 4, 5, 6)]
+        [DataRow("Red", 5, 6, 7, 8)]
+        [DataRow("Red", 7, 8, 9, 6)]
+        public void DiagonalValidMove(string color, int oldRow, int oldCol, int newRow, int newCol)
+        {
+            Assert.IsTrue(
+                MoveIsValid(color, oldRow, oldCol, newRow, newCol),
+                "Expected: Elephant Diagonal Valid Move to be Valid"
+            );
+        }
+
+        [TestMethod]
+        [DataRow("Black", 0, 2, 1, 1)]
+        [DataRow("Black", 0, 2, 3, 5)]
+        [DataRow("Black", 0, 2, 4, 6)]
+        public void DiagonalInvalidMove(string color, int oldRow, int oldCol, int newRow, int newCol)
+        {
+            Assert.IsFalse(
+                MoveIsValid(color, oldRow, oldCol, newRow, newCol),
+                "Expected: Elephant Diagonal Move of wrong size to be Valid"
+            );
+        }
+
+        [TestMethod]
+        [DataRow("Black", 0, 2, 2, 2)]
+        [DataRow("Black", 0, 2, 0, 0)]
+        [DataRow("Black", 0, 2, 2, 1)]
+        public void NonDiagonalMove(string color, int oldRow, int oldCol, int newRow, int newCol)
+        {
+            Assert.IsFalse(
+                MoveIsValid(color, oldRow, oldCol, newRow, newCol),
+                "Expected: Elephant Non Diagonal Move to be Invalid"
+            );
+        }
+
+        [TestMethod]
+        [DataRow("Black", 4, 2, 6, 4)]
+        [DataRow("Black", 4, 2, 6, 0)]
+        [DataRow("Red", 5, 6, 3, 4)]
+        [DataRow("Red", 5, 6, 3, 0)]
+        public void OverTheRiver(string color, int oldRow, int oldCol, int newRow, int newCol)
+        {
+            Assert.IsFalse(
+                MoveIsValid(color, oldRow, oldCol, newRow, newCol),
+                "Expected: Elephant Over the River Move to be Invalid"
+            );
+        }
+
+
+        [TestMethod]
+        [DataRow("Black", 0, 2, 2, 0, 1, 1)]
+        [DataRow("Red", 9, 6, 7, 4, 8, 5)]
+        public void DiagonalMove_WithBlocking_Invalid(string color, int oldRow, int oldCol, int newRow, int newCol, int blockRow, int blockCol)
+        {
+            Assert.IsFalse(
+                MoveIsValid(color, oldRow, oldCol, newRow, newCol, blockRow, blockCol),
+                "Expected: Elephant Move Through Blocking to be Invalid"
+            );
+        }
+    }
+}

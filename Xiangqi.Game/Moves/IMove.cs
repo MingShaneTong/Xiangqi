ba4183ef77ec16bb772d8406a1a23f6be2ba5927
MingShaneTong <63452934+MingShaneTong@users.noreply.github.com>
2023-01-20 23:30:45 +1300

    King facing invalid move (#13)
    * Added board has king facing
    
    * King Facing Move

--- Xiangqi.Game/Moves/IMove.cs
+++ Xiangqi.Game/Moves/IMove.cs
@@ -9,6 +9,6 @@ namespace Xiangqi.Game.Moves
     public interface IMove
     {
         public bool IsValid(Board board);
-        public void Apply(Board board);
+        public void Apply(Board board, bool validation = true);
     }
 }

86f72bc25061dcab5d4b0a7875182d129cf9107b
Ming Shane Tong <mstong800@gmail.com>
2023-01-24 22:40:12 +1300

    test created

--- Xiangqi.UnitTests/GameTests/IsInCheck.cs
+++ Xiangqi.UnitTests/GameTests/IsInCheck.cs
@@ -50,5 +50,45 @@ namespace Xiangqi.UnitTests.GameTests
 
             Assert.IsFalse(result, "Expected: Move to Self Check to be Invalid");
         }
+
+        [TestMethod]
+        [DataRow(Color.Red, "d4e4")]
+        public void Move_ResultingInBothCheck(Color color, string move)
+        {
+            string board =
+                    " | | | |k| | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | |r| | | | | \n" +
+                    " | | |R| | | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | |K| | | | | ";
+            bool result = TestSupport.MoveIsValid(board, color, move);
+
+            Assert.IsFalse(result, "Expected: Move to Both Check to be Invalid");
+        }
+
+        [TestMethod]
+        [DataRow(Color.Red, "e5f7")]
+        public void Move_ResultingInCheckAndKingFacing(Color color, string move)
+        {
+            string board =
+                    " | | | |k| | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | | |H| | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | | | | | | | \n" +
+                    " | | | |K| | | | ";
+            bool result = TestSupport.MoveIsValid(board, color, move);
+
+            Assert.IsFalse(result, "Expected: Move to Check and King Facing to be Invalid");
+        }
     }
 }

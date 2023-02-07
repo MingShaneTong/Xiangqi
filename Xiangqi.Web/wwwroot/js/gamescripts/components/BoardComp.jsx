e65a0f4a4b7b6ee8e438431c76b919793d47ac3b
Ming Shane Tong <mstong800@gmail.com>
2023-02-07 22:49:28 +1300

    Moved piece validation

--- Xiangqi.Web/wwwroot/js/gamescripts/components/BoardComp.jsx
+++ Xiangqi.Web/wwwroot/js/gamescripts/components/BoardComp.jsx
@@ -1,5 +1,13 @@
 ﻿let updateGame;
 
+function isAvailableMove(availableMoves, row, col) {
+	let moves = availableMoves.filter(m =>
+		m["Row"] == row && m["Col"] == col
+	);
+	let isMove = moves.length > 0;
+	return isMove;
+}
+
 class BoardComponent extends React.Component {
 	constructor(props) {
 		super(props);
@@ -31,20 +39,23 @@ class BoardComponent extends React.Component {
 		if (selectedPiece != null && selectedPiece.color == this.state.playerColor) {
 			// check if can move
 			if (clickedPiece == null || clickedPiece.color != this.state.playerColor) {
-				// TODO check if in valid moves
-				let move = {
-					GameId: this.state.gameId,
-					OldPosition: {
-						Row: this.state.selected.row,
-						Col: this.state.selected.col
-					},
-					NewPosition: {
-						Row: row,
-						Col: col
-					}
-				};
-				console.log(move);
-				movePiece(move);
+				// check if in valid moves
+				let isMove = isAvailableMove(this.state.moves, row, col);
+				if (isMove) {
+					console.log("Moving Piece");
+					let move = {
+						GameId: this.state.gameId,
+						OldPosition: {
+							Row: this.state.selected.row,
+							Col: this.state.selected.col
+						},
+						NewPosition: {
+							Row: row,
+							Col: col
+						}
+					};
+					movePiece(move);
+				}
 			}
 		}
 		this.setState({
@@ -55,8 +66,7 @@ class BoardComponent extends React.Component {
 
 	renderCell(row, col) {
 		let piece = this.state.board[row][col];
-		let moves = this.state.moves.filter(m => m["Row"] == row && m["Col"] == col);
-		let isMove = moves.length > 0;
+		let isMove = isAvailableMove(this.state.moves, row, col);
 		if (piece == null) {
 			return (<BlankComponent isMove={isMove} />);
 		} else {
